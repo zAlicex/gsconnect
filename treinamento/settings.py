64e1b8626b695991f99a9469066f7a2396f620ed
Alice Oliveira <aliceoliveira45130@gemail.com>
2025-07-18 11:00:02 -0300

    att

--- treinamento/settings.py
+++ treinamento/settings.py
@@ -124,13 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 STATICFILES_DIRS = [
     BASE_DIR / "core" / "static",
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 # Default primary key field type
